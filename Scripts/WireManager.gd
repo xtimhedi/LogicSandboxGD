9cbb07f1193977a21f1548fa903fac6e393a3efb
Madeline McWhorter <xtimhedi@gmail.com>
2026-05-11 07:16:29 -0500

    added the shit to spawn wires

---
+++ Scripts/WireManager.gd
@@ -0,0 +1,36 @@
+extends Node
+
+func SpawnWire(Start: Vector3, End: Vector3, WireColor: Color = Color.BLACK, Radius: float = 0.02) -> MeshInstance3D:
+	var NewMeshInstance = MeshInstance3D.new()
+	var NewCylinder = CylinderMesh.new()
+	
+	NewCylinder.top_radius = Radius
+	NewCylinder.bottom_radius = Radius
+	NewCylinder.radial_segments = 8
+	NewCylinder.rings = 1
+	
+	var NewMaterial = StandardMaterial3D.new()
+	NewMaterial.albedo_color = WireColor
+	NewCylinder.material = NewMaterial
+	NewMeshInstance.mesh = NewCylinder
+	
+	get_tree().current_scene.add_child(NewMeshInstance)
+	update_wire_position(NewMeshInstance, Start, End)
+	
+	return NewMeshInstance
+
+func update_wire_position(WireInstance: MeshInstance3D, Start: Vector3, End: Vector3):
+	var Direction: Vector3 = End - Start
+	var Length: float = Direction.length()
+	
+	if Length < 0.001: 
+		return
+		
+	WireInstance.mesh.height = Length
+	WireInstance.global_position = Start + (Direction * 0.5)
+	
+	WireInstance.global_transform = Transform3D(
+		Basis.looking_at(Direction, Vector3.UP), 
+		WireInstance.global_position
+	)
+	WireInstance.rotate_object_local(Vector3.RIGHT, deg_to_rad(90))
